753df1b701e5993deaf7ee90c554d729eac8e197
dani980boo <daniel980boo@gmail.com>
2023-08-31 09:03:47 -0300

    versao atualizada

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,11 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
-var somar = require("./modulos/dividir");
-var media = require("./modulos/multiplicar");
+var dividir = require("./modulos/dividir");
+var multiplicar = require("./modulos/multiplicar");
+var mensagem = require("./modulos/mensagem");
 
-console.log(somar(20,90));
-console.log(media(20,90));
-console.log(dividir(20,90));
-console.log(multiplicar(20,90));
+console.log("A soma de x e y é: " + somar(20,90));
+console.log("A média de x e y é: " + media(20,90));
+console.log("O produto de x e y é: " + dividir(20,90));
+console.log("A subtração de x e y é: " + multiplicar(20,90));
+console.log(mensagem + ("Alex"))
