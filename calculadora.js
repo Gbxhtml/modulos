9a0c041e346d5821b9d164454c158381fa33557c
Gabriel Espanhol <gabriel36pp@gmail.com>
2023-09-05 11:38:32 -0300

    mensagem

--- calculadora.js
+++ calculadora.js
@@ -1,11 +1,13 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var dividir = require("./modulos/dividir");
-var multiplicar = require("./modulos/multiplicar");
+var multiplicar= require("./modulos/multiplicar");
 var mensagem = require("./modulos/mensagem");
+var nome = "alex";
+var msg = "seja bem-vindo";
 
-console.log("A soma de x e y é: " + somar(20,90));
-console.log("A média de x e y é: " + media(20,90));
-console.log("O produto de x e y é: " + dividir(20,90));
-console.log("A subtração de x e y é: " + multiplicar(20,90));
-console.log(mensagem + ("Alex"))
+console.log( "a soma de x e y é: "+somar(20,90));
+console.log("a média de x e y é: "+media(20,90));
+console.log("o produto de x e y é: "+multiplicar(20,90));
+console.log("a subtrair de x e y é: "+dividir(20,90));
+console.log(mensagem(nome,msg));
--- modulos/mensagem.js
+++ modulos/mensagem.js
@@ -1,4 +1,4 @@
-var mensagem = function (nome) {
-    return "seja bem-vindo "+ nome;
-  }; 
-  module.exports = mensagem;
+var mensagem = function (nome,msg) {
+  return nome + " " + msg + "!";
+    }; 
+    module.exports = mensagem; 
